bfe4e1f3b957debc7525c6318294f0e7bbe0fc01
MartiArti <art97@bk.ru>
2023-03-26 01:52:44 +0300

    Yatube.API v2.3

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sorl.thumbnail',
     'rest_framework',
     'djoser',
     'api',
